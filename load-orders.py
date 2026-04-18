b3aa1ad3a5e54c559cc9448c72afcd64eb8ab331
LankyLuke97 <lukehowden@rocketmail.com>
2026-04-18 21:25:00 +0100

    Finalised load-orders; successfully load orders to Transaction table
    This was a bit of an ad-hoc script to explore interacting with records
    in the database, particularly inserting them; it's a one-off task to
    load old trades, and eventually these will be done through the app.
    
    It did flag something interesting about data decisions, simple as it is:
    are buys positive or negative cashflows in the database (and vice versa
    for sells)? What is the conceptual representation I want to encode in
    the data? For now, a sell is positive and a buy negative, as that
    indicates the change of money in the user's account.

--- load-orders.py
+++ load-orders.py
@@ -1,7 +1,13 @@
-from app.database_manager import open_connection_pool
+from sqlmodel import insert
+
+from app.database_manager import connect
 from app.schema import Transaction
 
 from datetime import datetime
+from dotenv import dotenv_values
+from decimal import Decimal
+
+env = dotenv_values('app/.env')
 
 with open('data/orders.csv', mode='r', encoding='utf-8') as orders:
     order_list = orders.readlines()
@@ -9,10 +15,15 @@ with open('data/orders.csv', mode='r', encoding='utf-8') as orders:
     transactions = []
     for order in order_list[1:]:
         const = {k:v for k, v in zip(headers, order.strip().split(','))}
-        if const['type'] == 'sell': const['quantity'] *= -1
+        const['price'] = Decimal(const['price'])
+        const['quantity'] = float(const['quantity'])
+        if const['type'] == 'buy': const['quantity'] *= -1
         del const['type']
-        const['datetime'] = datetime.strptime(const['datetime'], '%Y-%m-%d')
-        const['id'] = 0
-        transactions.append(Transaction(**const))
-    with open_connection_pool(connection_string='postgres://myuser:mypassword@localhost:5433/mydatabase') as db:
-        db.insert(Transaction).values(*transactions).run()
+        const['fees'] = Decimal(const['fees'])
+        const['timestamp'] = datetime.strptime(const['datetime'], '%Y-%m-%d')
+        del const['datetime']
+        transactions.append(const)
+    connection_string = f"{env['DB_PROTOCOL']}://{env['DB_USER']}:{env['DB_PASSWORD']}@{env['DB_HOST']}:{env['DB_PORT']}/{env['DB_NAME']}"
+    with connect(connection_string=connection_string) as session:
+        session.exec(insert(Transaction), params=transactions)
+        session.commit()
