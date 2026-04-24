3d42d6199926a44f7377a5f0f512200bdeec5523
LankyLuke97 <lukehowden@rocketmail.com>
2026-04-24 10:07:34 +0100

    Experiment with query parameter

--- app/main.py
+++ app/main.py
@@ -11,11 +11,9 @@ config = dotenv_values('.env')
 connection_string = f"{config['DB_PROTOCOL']}://{config['DB_USER']}:{config['DB_PASSWORD']}@{config['DB_HOST']}:{config['DB_PORT']}/{config['DB_NAME']}"
 
 @app.get('/transactions')
-async def about() -> list[Transaction]:
+async def about(ticker: str | None=None) -> list[Transaction]:
     with connect(echo=True, connection_string=connection_string) as session:
-        return session.exec(select(Transaction)).all()
+        statement = select(Transaction)
+        if ticker: statement = statement.where(Transaction.ticker == ticker)
+        return session.exec(statement).all()
 
-@app.get('/transactions/{ticker}')
-async def about(ticker: str) -> list[Transaction]:
-    with connect(echo=True, connection_string=connection_string) as session:
-        return session.exec(select(Transaction).where(Transaction.ticker == ticker)).all()
