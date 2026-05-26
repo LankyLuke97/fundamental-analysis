3262c726fd7822adcf858378626675e46f08b536
LankyLuke97 <lukehowden@rocketmail.com>
2026-05-26 19:13:13 +0100

    Refactor list item into own component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,14 @@
-import { useCallback, useEffect, useState } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 
 const DATA_ENDPOINT = 'http://localhost:8000';
-const VALID_API = ['transactions']
-
+const FILTERS = {ticker: '', dateFrom: '', dateTo: '',}
 const App = () => {
     const [transactionItems, setTransactionItems] = useState([]);
+    const [filters, setFilters] = useState(FILTERS)
 
     const fetchEndpoint = useCallback(async () => {
         try {
@@ -32,14 +32,21 @@ const App = () => {
 const List = ({items, children}) => (
     <>
     <ul>
-        {items.map(item => <li key={item.id}>
-            <span>{item.ticker}</span>&nbsp;
-            <span>{item.price}</span>&nbsp;
-            <span>{item.quantity}</span>&nbsp;
-            <span>{item.timestamp}</span>&nbsp;
-        </li>)}
+        {items.map(item => <ListItem key={item.id} item={item} />)}
     </ul>
     </>
 );
 
+const ListItem = ({item, children}) => (
+    <>
+        <li className="listBroad">
+            <span style={{ width: '20%' }}>{item.ticker}</span>&nbsp;
+            <span style={{ width: '15%' }}>{item.price}</span>&nbsp;
+            <span style={{ width: '15%' }}>{item.quantity}</span>&nbsp;
+            <span style={{ width: '50%' }}>{item.timestamp}</span>&nbsp;
+        </li>
+    </>
+);
+
 export default App
+
