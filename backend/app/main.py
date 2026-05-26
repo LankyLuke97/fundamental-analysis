d73491c1d990b9efee09b112055e426f6cd23335
LankyLuke97 <lukehowden@rocketmail.com>
2026-05-26 12:39:42 +0100

    Move backend into own folder. Add temporary CORS fix - needs further learning

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,6 +2,7 @@ from database_manager import connect
 from dotenv import dotenv_values
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from sqlmodel import select
 
 from schema import Transaction
@@ -10,6 +11,13 @@ app = FastAPI()
 config = dotenv_values('.env')
 connection_string = f"{config['DB_PROTOCOL']}://{config['DB_USER']}:{config['DB_PASSWORD']}@{config['DB_HOST']}:{config['DB_PORT']}/{config['DB_NAME']}"
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # or your frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.get('/transactions')
 async def about(ticker: str | None=None) -> list[Transaction]:
     with connect(echo=True, connection_string=connection_string) as session:
