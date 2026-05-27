c4ce07212ac3d9bc71d532c6585054717d1a9f7d
LankyLuke97 <lukehowden@rocketmail.com>
2026-05-27 22:36:02 +0100

    WIP useReducer for transaction item state

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useRef, useState } from 'react'
+import { useCallback, useEffect, useReducer, useRef, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
@@ -6,40 +6,88 @@ import './App.css'
 
 const DATA_ENDPOINT = 'http://localhost:8000/transactions'; {/* Currently transactions is the only endpoint */}
 const FILTERS = {ticker: '', dateFrom: '', dateTo: '',}
+
+const transactionActions = {
+    fetchInitial: 'FETCH_TRANSACTIONS',
+    fetchSuccess: 'FETCH_SUCCESS',
+    fetchFailure: 'FETCH_FAILURE',
+};
+
+const transactionReducer = (state, action) => {
+    switch (action.type) {
+        case transactionActions.fetchInitial:
+            return {
+                ...state,
+                isLoading: true,
+                isError: false,
+            };
+        case transactionActions.fetchSuccess:
+            return {
+                ...state,
+                isLoading: false,
+                isError: false,
+                data: action.payload,
+            };
+        case transactionActions.fetchFailure:
+            return {
+                ...state,
+                isLoading: false,
+                isError: true,
+            };
+        default:
+            throw new Error();
+    }
+};
+
 const App = () => {
-    const [transactionItems, setTransactionItems] = useState([]);
+    console.log('Starting render');
     const [filters, setFilters] = useState(FILTERS);
     const [url, setUrl] = useState(DATA_ENDPOINT);
+    const [transactionItems, dispatchTransactionItems] = useReducer(transactionReducer, {
+        data: [],
+        isLoading: false,
+        isError: false,
+    });
 
+    console.log(filters, url, transactionItems);
     const handleSearchInput = (key, value) => setFilters({...filters, [key]: value});
     const handleSearchSubmit = event => {
         let queryString = new URLSearchParams(Object.entries(filters).filter(([_, value]) => value != null && value.trim() != '')).toString();
         queryString = queryString ? `?${queryString}` : '';
         setUrl(`${DATA_ENDPOINT}${queryString}`);
-        event.preventDefault();
+        event.preventDEfault();
     };
 
+    console.log(filters, url, transactionItems);
     const fetchEndpoint = useCallback(async () => {
+        dispatchTransactionItems({ type: transactionActions.fetchInitial });
         try {
             const response = await fetch(url);
             const result = await response.json();
-            setTransactionItems(result);
+            dispatchTransactionItems({ 
+                payload: result,
+                type: transactionActions.fetchSuccess,
+            });
+            console.log('Fetched data from', url);
         } catch (error) {
+            dispatchTransactionItems({ type: transactionActions.fetchFailure });
             console.log(`Error while downloading from ${DATA_ENDPOINT}:\n`, error);
         }
     }, [url]);
+    console.log(filters, url, transactionItems);
 
     useEffect(() => {fetchEndpoint()}, [fetchEndpoint]);
 
+    console.log(filters, url, transactionItems);
     return (
         <>
             <SearchForm filters={filters} onSearchInput={handleSearchInput} onSearchSubmit={handleSearchSubmit} />
-            <List items={transactionItems} />
+            <List items={transactionItems.data} />
         </>
     );
 };
 
-const SearchForm = ({ filters, onSearchInput, onSearchSubmit }) => (
+const SearchForm = ({ filters, onSearchInput, onSearchSubmit }) => console.log('rendering form') || (
     <form onSubmit={onSearchSubmit} className="search-form">
         {Object.entries(filters).map(([key, value]) => 
             <InputWithLabel key={key} id={`filter${key}`} value={value} isFocused onInputChange={(e) => onSearchInput(key, e.target.value)}>{key}:&nbsp;</InputWithLabel>
@@ -65,7 +113,7 @@ const InputWithLabel = ({id, value, type='text', onInputChange, isFocused, child
     );
 };
 
-const List = ({items, children}) => (
+const List = ({items, children}) => console.log('rendering list') ||(
     <>
     <ul>
         {items.map(item => <ListItem key={item.id} item={item} />)}
@@ -73,7 +121,7 @@ const List = ({items, children}) => (
     </>
 );
 
-const ListItem = ({item, children}) => (
+const ListItem = ({item, children}) => console.log(item) ||(
     <>
         <li className="listBroad">
             <span style={{ width: '20%' }}>{item.ticker}</span>&nbsp;
