2a50cabe184dcd6b0e17c6249462811e2c310bcf
LankyLuke97 <lukehowden@rocketmail.com>
2026-08-02 21:21:53 +0100

    Comment out some not-important details in main

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,15 +1,12 @@
-from database_manager import connect
+# from database_manager import connect
 from dotenv import dotenv_values
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from sqlmodel import select
-
-from schema import Transaction
 
 app = FastAPI()
 config = dotenv_values(".env")
-connection_string = f"{config['DB_PROTOCOL']}://{config['DB_USER']}:{config['DB_PASSWORD']}@{config['DB_HOST']}:{config['DB_PORT']}/{config['DB_NAME']}"
+# connection_string = f"{config['DB_PROTOCOL']}://{config['DB_USER']}:{config['DB_PASSWORD']}@{config['DB_HOST']}:{config['DB_PORT']}/{config['DB_NAME']}"
 
 app.add_middleware(
     CORSMiddleware,
@@ -19,7 +16,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
+"""
 @app.get("/transactions")
 async def about(ticker: str | None = None) -> list[Transaction]:
     with connect(echo=True, connection_string=connection_string) as session:
@@ -27,3 +24,4 @@ async def about(ticker: str | None = None) -> list[Transaction]:
         if ticker:
             statement = statement.where(Transaction.ticker == ticker)
         return list(session.exec(statement).all())
+"""
