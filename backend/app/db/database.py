30a3babd9cc61ed746a24fa658f7bbb1a4852ebb
LankyLuke97 <lukehowden@rocketmail.com>
2026-08-02 21:23:25 +0100

    Basic session creation, helper script to import all schemas

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -3,11 +3,17 @@ from sqlmodel import create_engine, Session, SQLModel
 # When creating the database tables with SQLModel.metadata.create_all,
 # the models must first be imported to register them. This is not needed
 # when using Alembic (or equivalent).
-from app.db.schemas import cash_flow, cash_flow_tag, cash_flow_category  # noqa : F401
+from app.db.schema import all  # noqa: F401
 
 
 # This is temporary 'up-and-running' code to test some of the models
 sqlite_url = "sqlite://"
-engine = create_engine(sqlite_url, echo=True)
+engine = create_engine(sqlite_url, echo=False)
 
 SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    with Session(engine) as session:
+        print("Getting real database session")
+        yield session
