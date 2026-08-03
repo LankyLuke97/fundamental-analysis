8e28c982059f172cf6b2b556acdd6f4707e2f96a
LankyLuke97 <lukehowden@rocketmail.com>
2026-08-03 21:04:19 +0100

    Working further on services and tests; error
    There's an error during teardown of the add_categories fixture. This is
    because I have added cashflows in the session, which have a foreign key
    on a category id which can't be null. The fixture teardown deletes the
    categories - but this leaves a null foreign key in the cashflow, which
    has been added but not deleted, since now it's outside of the fixture.
    
    I need to look into unit testing databases here. This is probably a good
    argument for the repository pattern, since then I could have a fake
    repository that just gets created anew for every test. Another 'easy'
    fix would be to create a fresh in-memory database for every test and not
    worry about cleanup; I don't know if this is good practice.

--- backend/app/service/cash_flow.py
+++ backend/app/service/cash_flow.py
@@ -2,10 +2,6 @@ from sqlmodel import Session, select
 
 from app.db.schema.cash_flow import (
     CashFlow,
-    CashFlowCreate,
-    CashFlowRead,
-    CashFlowUpdate,
-    CashFlowDelete,
 )
 
 
@@ -16,6 +12,14 @@ class CashFlowNotFound(Exception):
 class CashFlowService:
     def __init__(self, session: Session):
         self._db = session
+
+    def add_cash_flow(self, cash_flow: CashFlow) -> CashFlow:
+        self._db.add(cash_flow)
+        self._db.commit()
+        self._db.refresh(cash_flow)
+        if not cash_flow or not cash_flow.id:
+            raise Exception  # to-do
+        return cash_flow
 
     def get_cash_flow(self, cash_flow_id: int) -> CashFlow:
         query = select(CashFlow).where(CashFlow.id == cash_flow_id)
--- backend/tests/service/test_cash_flow.py
+++ backend/tests/service/test_cash_flow.py
@@ -1,13 +1,15 @@
+from contextlib import contextmanager
+from typing import Iterable
 from decimal import Decimal
 
-from fastapi import Depends
-from pytest import fixture
+from pytest import fixture, raises
+from sqlmodel import SQLModel, Session
 
 from app.db.database import get_session
 from app.db.schema.cash_flow import CashFlow
 from app.db.schema.cash_flow_category import CashFlowCategory
 from app.main import app
-from app.service.cash_flow import CashFlowService
+from app.service.cash_flow import CashFlowNotFound, CashFlowService
 
 from tests.test_database import get_test_session
 
@@ -15,21 +17,93 @@ from tests.test_database import get_test_session
 app.dependency_overrides[get_session] = get_test_session
 
 
+@contextmanager
+def temp_database_data(data: Iterable[SQLModel], session: Session):
+    for datum in data:
+        session.add(datum)
+    session.commit()
+    for datum in data:
+        session.refresh(datum)
+    try:
+        yield data
+    finally:
+        for datum in data:
+            session.delete(datum)
+        session.commit()
+
+
 @fixture
-def add_cash_flow():
-    pass
+def session():
+    with get_test_session() as session:
+        yield session
 
 
-def test_get_cash_flow():
-    session = get_test_session()
-    category = CashFlowCategory(name="Test Category")
-    session.add(category)
-    session.commit()
-    session.refresh(category)
-    cash_flow = CashFlow(value=Decimal("100.00"), category_id=(category.id))
-    session.add(cash_flow)
-    session.commit()
-    session.refresh(cash_flow)
-    service = CashFlowService(session=session)
-    returned_cash_flow = service.get_cash_flow(cash_flow.id)
-    assert cash_flow == returned_cash_flow
+@fixture
+def service(session):
+    yield CashFlowService(session=session)
+
+
+@fixture
+def add_categories(session):
+    categories = [
+        CashFlowCategory(
+            name="Test category 1",
+        ),
+        CashFlowCategory(
+            name="Test category 2",
+            description="Optional category description",
+        ),
+        CashFlowCategory(
+            name="Test category 3",
+        ),
+    ]
+
+    with temp_database_data(categories, session) as data:
+        yield data
+
+
+@fixture
+def add_cash_flows(session, add_categories):
+    categories = add_categories
+    cash_flows = [
+        CashFlow(
+            value=Decimal("100.00"),
+            category_id=categories[0].id,
+        ),
+        CashFlow(
+            value=Decimal("75.00"),
+            category_id=categories[0].id,
+        ),
+        CashFlow(
+            value=Decimal("10.00"),
+            category_id=categories[1].id,
+        ),
+    ]
+    with temp_database_data(cash_flows, session) as data:
+        yield data
+
+
+def test_get_cash_flow(service, add_cash_flows):
+    expected = add_cash_flows[0]
+    retrieved = service.get_cash_flow(expected.id)
+    assert retrieved == expected
+
+
+def test_get_missing_cash_flow(service, add_cash_flows):
+    with raises(CashFlowNotFound):
+        service.get_cash_flow(-1)
+
+
+def test_add_cash_flow(service, add_categories):
+    category = add_categories[0]
+
+    cash_flow = service.add_cash_flow(
+        CashFlow(
+            value=Decimal("200.00"),
+            category_id=category.id,
+        )
+    )
+
+    assert cash_flow.id is not None
+    assert Decimal("200.00") == cash_flow.value
+    assert category.id == cash_flow.category_id
