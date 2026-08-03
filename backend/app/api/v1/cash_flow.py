584703a4a9432b799fd75b93472f619863079fc2
LankyLuke97 <lukehowden@rocketmail.com>
2026-08-03 21:03:03 +0100

    Switch to synchronous SQLite interactions

--- backend/app/api/v1/cash_flow.py
+++ backend/app/api/v1/cash_flow.py
@@ -1,7 +1,6 @@
 from starlette.status import HTTP_204_NO_CONTENT
-from typing import Sequence, Any
-from fastapi import APIRouter, Depends, HTTPException
-from sqlmodel import Session
+from typing import Sequence
+from fastapi import APIRouter, Depends
 
 from app.db.schema.cash_flow import (
     CashFlowCreate,
@@ -9,7 +8,7 @@ from app.db.schema.cash_flow import (
     CashFlowUpdate,
 )
 from app.db.database import get_session
-from app.services.cash_flow_service import CashFlowService
+from app.service.cash_flow import CashFlowService
 
 
 router = APIRouter(prefix="cash_flows")
@@ -20,36 +19,35 @@ def get_cash_flow_service() -> CashFlowService:
 
 
 @router.get("/{cash_flow_id}", response_model=CashFlowRead)
-async def get_cash_flow(
+def get_cash_flow(
     cash_flow_id: int, service: CashFlowService = Depends(get_cash_flow_service)
 ):
-    return await service.get_cash_flow(cash_flow_id)
+    return service.get_cash_flow(cash_flow_id)
 
 
 @router.get("", response_model=Sequence[CashFlowRead])
-async def get_cash_flows(service: CashFlowService = Depends(get_cash_flow_service)):
-    return await service.list_cash_flows()
+def get_cash_flows(service: CashFlowService = Depends(get_cash_flow_service)):
+    return service.list_cash_flows()
 
 
 @router.post("", response_model=CashFlowRead)
-async def create_cash_flow(
+def create_cash_flow(
     cash_flow: CashFlowCreate, service: CashFlowService = Depends(get_cash_flow_service)
 ):
-    return await service.add_cash_flow(cash_flow)
+    return service.add_cash_flow(cash_flow)
 
 
 @router.patch("/{cash_flow_id}", response_model=CashFlowRead)
-async def update_cash_flow(
+def update_cash_flow(
     cash_flow_id: int,
     cash_flow: CashFlowUpdate,
     service: CashFlowService = Depends(get_cash_flow_service),
 ):
-    return await service.update_cash_flow(cash_flow_id, cash_flow)
+    return service.update_cash_flow(cash_flow_id, cash_flow)
 
 
 @router.delete("/{cash_fllow_id}", status_code=HTTP_204_NO_CONTENT)
-async def delete_cash_flow(
+def delete_cash_flow(
     cash_flow_id: int, service: CashFlowService = Depends(get_cash_flow_service)
 ):
-    return await service.delete_cash_flow(cash_flow_id)
-    l
+    return service.delete_cash_flow(cash_flow_id)
