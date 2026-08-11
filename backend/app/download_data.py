99b6e41eb0af682d1963b6b32451a30af34585dd
LankyLuke97 <lukehowden@rocketmail.com>
2026-08-11 21:33:16 +0100

    Layout changes to old download script

--- backend/app/download_data.py
+++ backend/app/download_data.py
@@ -4,9 +4,22 @@ from httpx import AsyncClient, Headers
 from typing import cast, Any, Mapping, Sequence, TypeAlias
 
 config: dict[str, str] = {}
-AsyncClientHeader: TypeAlias = Headers | Mapping[str, str] | Mapping[bytes, bytes] | Sequence[tuple[str, str]] | Sequence[tuple[bytes, bytes]] | None 
+AsyncClientHeader: TypeAlias = (
+    Headers
+    | Mapping[str, str]
+    | Mapping[bytes, bytes]
+    | Sequence[tuple[str, str]]
+    | Sequence[tuple[bytes, bytes]]
+    | None
+)
 
-async def download_one(client: AsyncClient, url: str, headers: AsyncClientHeader, params: dict[str, str | int] | None=None) -> Any:
+
+async def download_one(
+    client: AsyncClient,
+    url: str,
+    headers: AsyncClientHeader,
+    params: dict[str, str | int] | None = None,
+) -> Any:
     headers = headers or {}
     params = params or {}
     try:
@@ -18,25 +31,34 @@ async def download_one(client: AsyncClient, url: str, headers: AsyncClientHeader
     print(f"Downloaded successfully from {response.url}")
     return params
 
+
 def download_many(base_endpoint: str, tickers: list[str]) -> int:
     return asyncio.run(supervisor(base_endpoint, tickers))
 
+
 async def supervisor(base_endpoint: str, tickers: list[str]) -> int:
-    headers = {'apikey': config['API_FMP']}
+    headers = {"apikey": config["API_FMP"]}
     async with AsyncClient() as client:
-        download_coroutines = [download_one(client, base_endpoint, headers, {'symbol': ticker}) for ticker in tickers]
+        download_coroutines = [
+            download_one(client, base_endpoint, headers, {"symbol": ticker})
+            for ticker in tickers
+        ]
         res = await asyncio.gather(*download_coroutines, return_exceptions=True)
     print(res)
 
     return len(res)
 
+
 def main() -> None:
-    tickers = ['AAPL', 'GAW.L', 'META', 'BABA']
-    download_many('https://financialmodelingprep.com/stable/historical-price-eod/light', tickers)
+    tickers = ["AAPL", "GAW.L", "META", "BABA"]
+    download_many(
+        "https://financialmodelingprep.com/stable/historical-price-eod/light", tickers
+    )
+
 
 if __name__ == "__main__":
     config = cast(dict[str, str], dotenv_values(".env"))
     for k in config:
-        if not config[k]: del config[k]
+        if not config[k]:
+            del config[k]
     main()
-
